c0a004200a1ded778b24690bde5a98abfce3d4f3
Lilian <lilianpeuron@gmail.com>
2023-10-23 23:15:34 +0200

    upd img cards

--- src/components/cardSkillList/CardSkillList.jsx
+++ src/components/cardSkillList/CardSkillList.jsx
@@ -4,12 +4,12 @@ import s from './CardSkill.module.scss'
 const CardSkillList = () => {
     return (
         <ul className={s.CardSkillList}>
-            <CardSkill img='./images/projects/resaweb_card.jpg' >Site MMI</CardSkill>
-            <CardSkill img='./images/projects/resaweb_card.jpg' url='/resaweb' >Résaweb</CardSkill>
-            <CardSkill img='./images/projects/resaweb_card.jpg' >Yonnha</CardSkill>
-            <CardSkill img='./images/projects/dataviz_card.jpg' url='/dataviz' >DataViz</CardSkill>
-            <CardSkill img='./images/projects/vovinam_card.jpg' url='/vovinam' >Vovinam</CardSkill>
-            <CardSkill img='./images/projects/siteonepage_card.jpg' url='/daftpunk' >Daft-Punk</CardSkill>
+            <CardSkill img='./images/cards_projects/card_img_resaweb.jpg' >Site MMI</CardSkill>
+            <CardSkill img='./images/cards_projects/card_img_resaweb.jpg' url='/resaweb' >Résaweb</CardSkill>
+            <CardSkill img='./images/cards_projects/card_img_resaweb.jpg' >Yonnha</CardSkill>
+            <CardSkill img='./images/cards_projects/card_img_dataviz.jpg' url='/dataviz' >DataViz</CardSkill>
+            <CardSkill img='./images/cards_projects/card_img_vovinam.jpg' url='/vovinam' >Vovinam</CardSkill>
+            <CardSkill img='./images/cards_projects/card_img_daftpunk.jpg' url='/daftpunk' >Daft-Punk</CardSkill>
         </ul>
     )
 }
