57c031a56f35b1337918751a6132256b0f7817e0
Lilian <lilianpeuron@gmail.com>
2023-10-25 00:20:47 +0200

    upd gsap vovinam

--- src/screens/Vovinam.jsx
+++ src/screens/Vovinam.jsx
@@ -8,8 +8,38 @@ import SkillTitle from "../components/skillTable/skillTitle/SkillTitle"
 import TextZone from "../components/textZone/TextZone"
 import Contact from "../partials/home/contact/Contact"
 import s from './SingleProject.module.scss'
+import gsap from 'gsap'
+import { useRef, useEffect } from "react"
 
 const Vovinam = () => {
+
+    useEffect(() => {
+        const tl = gsap.timeline()
+
+        tl.fromTo( "#gsap_left_project" ,{
+            opacity : 0,
+            y : 50
+        },
+        {
+            opacity : 1,
+            y : 0,
+            duration : 0.5,
+            delay : 0.5
+        })
+
+        tl.fromTo( "#gsap_right_project" ,{
+            opacity : 0,
+            y : 50
+        },
+        {
+            opacity : 1,
+            y : 0,
+            duration : 0.5,
+            delay : -0.2
+        })
+
+    }, [])
+
     const projectSkills = [
         "Premiere Pro",
     ]
@@ -23,10 +53,10 @@ const Vovinam = () => {
             <SectionPage style="column" sectionPT ="40px">
                 <SectionTitle>Vovinam</SectionTitle>
                 <div className={s.SingleProject}>
-                    <div>
+                    <div id="gsap_left_project">
                         <ImgBlock imgWidth="100%" imgHeight="100%" imgURL='./images/projects/vovinam_img.jpg'/>
                     </div>
-                    <div>
+                    <div id="gsap_right_project">
                         <TextZone title="Objectif">
                             Réalisation d'une vidéo résumé d'environ 7 mn sur l'ensemble de l'évènement (installation, épreuves et grande finale).
                         </TextZone>
