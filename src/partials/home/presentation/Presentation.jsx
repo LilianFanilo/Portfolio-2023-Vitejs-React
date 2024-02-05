fdba47ca17616b40f82eeeb3d3b7fd36a5ee1684
Lilian <lilianpeuron@gmail.com>
2024-02-05 10:17:17 +0100

    upd presentation

--- src/partials/home/presentation/Presentation.jsx
+++ src/partials/home/presentation/Presentation.jsx
@@ -14,7 +14,7 @@ const Presentation = () => {
                     Qui suis-je ?
                 </SectionTitle>
                 <TextZone>
-                20 ans - À la recherche d’un contrat d’alternance pour 2023-2024. <br />
+                20 ans - À recherche d’un stage de 3 mois minimum débutant le plus tôt possible. <br />
                 Mon stage de développeur web chez Peexeo m’a permis de renforcer mes compétences en 
                 programmation, de participer à des projets clients et d'acquérir une expérience concrète dans 
                 le domaine du développement web, mais aussi de partager des idées et d'échanger au sein 
